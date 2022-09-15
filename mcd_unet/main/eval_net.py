5b66a9d942650726ff1e6acbed80ad1f653a44d4
smiyaki <miyaki@v101.fun.bio.keio.ac.jp>
2022-09-15 21:09:33 +0900

    eval support raw

--- mcd_unet/main/eval_net.py
+++ mcd_unet/main/eval_net.py
@@ -12,8 +12,14 @@ from skimage import io
 import statistics
 import os
 
-def eval_unet( device, test_list, model=None, net_g=None, net_s=None, net_s_another=None, use_mcd=False, logfilePath=None):
+def eval_mcd( device, test_list, model=None, net_g=None, net_s=None, net_s_another=None, raw=False, logfilePath=None):
     IoU_list = []
+
+    tf = transforms.Compose([
+                transforms.ToPILImage(),
+                #transforms.Resize(h),
+                transforms.ToTensor()
+            ])
 
     for i, image in enumerate(test_list):
 
@@ -21,32 +27,29 @@ def eval_unet( device, test_list, model=None, net_g=None, net_s=None, net_s_anot
         gt = torch.from_numpy(image[1][0]).cuda(device)
     
         with torch.no_grad():
-            if use_mcd:
+            if raw:
+                mask = model(img)
+
+                mask_prob = torch.sigmoid(mask).squeeze(0)
+                mask_prob = tf(mask_prob.cuda(device))
+                inf = mask_prob.squeeze().cuda(device)
+
+            else:
                 feat = net_g(img)
                 mask = net_s(*feat)
-                if net_s_another != None:
-                    mask_ano = net_s_another(*feat)
-            else:
-                mask = model(img)
-                
-            mask_prob = torch.sigmoid(mask).squeeze(0)
-            
-            tf = transforms.Compose([
-                transforms.ToPILImage(),
-                #transforms.Resize(h),
-                transforms.ToTensor()
-            ])
-        
-            mask_prob = tf(mask_prob.cuda(device))
-            inf_s1 = mask_prob.squeeze().cuda(device)
+                mask_ano = net_s_another(*feat)
 
+                mask_prob = torch.sigmoid(mask).squeeze(0)
+                mask_prob_ano = torch.sigmoid(mask_ano).squeeze(0)
 
-            mask_prob_ano = torch.sigmoid(mask_ano).squeeze(0)
-            mask_prob_ano = tf( mask_prob_ano.cuda(device) )
-            inf_s2 = mask_prob_ano.squeeze().cuda(device)
+                mask_prob = tf(mask_prob.cuda(device))
+                mask_prob_ano = tf( mask_prob_ano.cuda(device) )
 
+                inf_s1 = mask_prob.squeeze().cuda(device)
+                inf_s2 = mask_prob_ano.squeeze().cuda(device)
 
-            inf = (inf_s1 + inf_s2) /2
+                inf = (inf_s1 + inf_s2) /2
+
 
         tmp_IoU = iou_pytorch(inf, gt, device)
         
@@ -88,8 +91,12 @@ if __name__ == '__main__':
     if args.raw_mode:
         # load U-Net
         net = UNet(first_num_of_kernels=args.first_num_of_kernels, n_channels=1, n_classes=1, bilinear=True)
+        #print( checkPoint.keys() )
+        #net.load_state_dict( checkPoint['best_net'] )
+        net.load_state_dict( checkPoint )
         net.to(device=device)
         net.eval()
+        net_g=None; net_s1=None; net_s2=None
 
     else:
         # load MCD-U-Net
@@ -105,8 +112,10 @@ if __name__ == '__main__':
         net_s2.load_state_dict( checkPoint['best_s2'] )
         net_s2.to(device=device)
         net_s2.eval()
+
+        net=None
     
-    testDir = f'/home/miyaki/unsupdomaada_for_semaseg_of_cell_images/LIVECell_dataset/test_data/{args.cell}'
+    testDir = f'/home/miyaki/unsupdomaada_for_semaseg_of_cell_images/LIVECell_dataset/train_data/{args.cell}/cat_train'
     testFiles = glob.glob(f'{testDir}/*')
     
     tests = create_trainlist( testFiles, test=1, cut=1 )
@@ -138,7 +147,7 @@ if __name__ == '__main__':
     os.makedirs( dir_imgs, exist_ok=True )
     path_w = f'{dir_result}/evaluation.txt'
     #net_s_another=net_s2,
-    IoU = eval_unet( device, tests, net_g=net_g, net_s=net_s1, net_s_another=net_s2, use_mcd=1, logfilePath=path_w)
+    IoU = eval_mcd( device, tests, model=net, net_g=net_g, net_s=net_s1, net_s_another=net_s2, raw=args.raw_mode , logfilePath=path_w)
     
     #img_result, img_merge = segment(seg_shsy5y, net_g=net_g, net_s=net_s1, use_mcd=1)
     
