8b230c8d495237fbde07d5b4bac92fb8424d02b6
smiyaki <enzo3.shintaro.328@gmail.com>
2022-12-12 18:40:23 +0900

    slightly cot

--- co_teaching_unet/train_co_teaching.py
+++ co_teaching_unet/train_co_teaching.py
@@ -303,6 +303,7 @@ def train_net(net_1,
 
                 tgt_seg_epoch_loss_1 += t_loss_1.item()
                 tgt_seg_epoch_loss_2 += t_loss_2.item()
+                
                 epoch_loss_1 += loss_1.item()
                 epoch_loss_2 += loss_2.item()
                 count += 1
